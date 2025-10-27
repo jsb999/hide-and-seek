5ebe53dfb2991ffcb42bc4659aae403d57a22bed
jsb999 <90780236+jsb999@users.noreply.github.com>
2025-10-27 16:57:10 +0000

    tests

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -2,10 +2,53 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
 
 class AppTest {
   @Test
-  void theTestsWork() {
-    assertEquals(true, true);
+  public void testLinearSearchOnSortedList() {
+    FuzzyListGenerator list = new FuzzyListGenerator();
+    FuzzyFinder finder = new FuzzyFinder();
+    ArrayList<Fuzzy> sortedFuzzies = list.sortedRainbowFuzzies();
+
+    int index = finder.linearSearch(sortedFuzzies);
+
+    assertNotEquals(-1, index);
+    assertEquals("gold", sortedFuzzies.get(index).color);
+  }
+  
+  @Test
+  public void testLinearSearchOnRandomList() {
+    FuzzyListGenerator list = new FuzzyListGenerator();
+    FuzzyFinder finder = new FuzzyFinder();
+    ArrayList<Fuzzy> sortedFuzzies = list.randomizedRainbowFuzzies();
+
+    int index = finder.linearSearch(sortedFuzzies);
+
+    assertNotEquals(-1, index);
+    assertEquals("gold", sortedFuzzies.get(index).color);
+  }
+
+  @Test
+  public void testBinarySearchOnRandomList() {
+    FuzzyListGenerator list = new FuzzyListGenerator();
+    FuzzyFinder finder = new FuzzyFinder();
+    ArrayList<Fuzzy> random = list.randomizedRainbowFuzzies();
+
+    int index = finder.binarySearch(random);
+
+    assertEquals(-1, index);
+  }
+
+  @Test
+  public void testBinarySearchOnSortedList() {
+    FuzzyListGenerator list = new FuzzyListGenerator();
+    FuzzyFinder finder = new FuzzyFinder();
+    ArrayList<Fuzzy> random = list.sortedRainbowFuzzies();
+
+    int index = finder.binarySearch(random);
+
+    assertNotEquals(-1, index);
+    assertEquals("gold", sortedFuzzies.get(index).color);
   }
 }
